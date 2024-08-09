1241765e29a27aa3410b466385ac791a191fad5a
anabellechan <97331839+anabellechan@users.noreply.github.com>
2024-08-09 16:23:57 +0800

    Exception handling try catch finally block

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,5 +1,17 @@
 class ExceptionHandling {
     public static void main(String[] args){
-        
+        try{
+           getInt();
+        }
+        catch(NumberFormatException | NullPointerException e) {
+            System.out.println("Unable to make integer out of that input.");
+    }   
+        finally {
+            System.out.println("In the finally block");
+        }
+    System.out.println("End here");
+}
+    private static void getInt() {
+        int myInt=Integer.parseInt("abc");
     }
 }
